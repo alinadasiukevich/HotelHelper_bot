cac7f71be41a658132f012f1baa6260d07ab183d
Alina Dasiukevich <alinadasiukevich@gmail.com>
2021-12-27 21:59:51 +0300

    Диплом. Часть bestdeal.py

---
+++ config.py
@@ -0,0 +1,6 @@
+token = '5018171733:AAGV7p-SOMwCefUN8LzyL_N6KC8wfV3N6lA'
+
+headers = {
+                'x-rapidapi-host': "hotels4.p.rapidapi.com",
+                'x-rapidapi-key': "c69c1872abmshf497846dcd25465p192f04jsn091745b8c356"
+            }
